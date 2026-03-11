f6e792430d547c94029256c6ad5b405fb4426fe6
Ben Rosenthal <benrosenthal03@gmail.com>
2026-03-11 11:23:52 +0100

    filter clusters for visualization, write plot script

---
+++ src/scripts/plots.py
@@ -0,0 +1,67 @@
+import polars
+import matplotlib.pyplot as plt
+
+def plot_percentage_in_puls_over_genome_length(clusters_table_filtered):
+    figure, axs = plt.subplots(1, figsize=(8, 6))
+    axs.scatter(x=clusters_table_filtered.select('length'), y=clusters_table_filtered.select('percentage_in_puls'))
+    axs.set_xscale("log")
+    axs.set_xlim(1e4, 1e7)
+    axs.set_title("Percentage of genome in PULs over genome length")
+    axs.set_xlabel("Genome Length")
+    axs.set_ylabel("Percentage of genome in PULs")
+
+    # vertical line at 50.000kb
+    plt.vlines(50000, ymin=0, ymax=55, color="red")
+    plt.savefig("src/data/plots/temp.png", dpi=300)
+
+
+def get_taxonomic_counts(clusters_table_filtered, rank="phylum", cutoff=10):
+    return (
+        clusters_table_filtered
+        .group_by(rank)
+        .len()  # or .count() on older Polars
+        .rename({"len": "count"})
+        .with_columns(
+            polars.when(polars.col("count") < cutoff)
+            .then(polars.lit("other"))
+            .otherwise(polars.col(rank))
+            .alias(f"{rank}_group")
+        )
+        .group_by(f"{rank}_group")
+        .agg(polars.col("count").sum().alias("count"))
+        .fill_null("Unknown")
+        .sort("count", descending=True)
+    )
+
+def plot_taxonomic_distributions(clusters_table_filtered):
+    phylum_counts = get_taxonomic_counts(clusters_table_filtered, rank="phylum", cutoff=10)
+    class_counts = get_taxonomic_counts(clusters_table_filtered, rank="class", cutoff=5)
+    order_counts = get_taxonomic_counts(clusters_table_filtered, rank="order", cutoff=5)
+    family_counts = get_taxonomic_counts(clusters_table_filtered, rank="family", cutoff=5)
+
+    figure, axs = plt.subplots(2, 2, figsize=(16, 12))
+    for ax, counts, rank in zip(axs.flatten(), [phylum_counts, class_counts, order_counts, family_counts], ["Phylum", "Class", "Order", "Family"]):
+        x = counts.select(f"{rank.lower()}_group").to_series()
+        heights = counts.select("count").to_series()
+        ax.pie(heights, labels=x, radius=1, wedgeprops=dict(width=0.3, edgecolor='w'))
+        ax.set_title(f"Taxonomic distribution on {rank} level")
+
+    plt.tight_layout()
+    plt.savefig("src/data/plots/temp.png")
+    
+    # DRAFT OF NESTED PIE CHARTS FOR PHYLUM AND CLASS, BUT SORTING FUCKS IT UP A BIT
+    # fig, ax = plt.subplots()
+    # size = 0.3
+    # ax.pie(phylum_counts.select("count").to_series(), radius=1,
+    #     wedgeprops=dict(width=size, edgecolor='w'),
+    #     labels=phylum_counts.select(f"phylum_group").to_series())
+
+    # ax.pie(class_counts.select("count").to_series(), radius=1-size,
+    #     wedgeprops=dict(width=size, edgecolor='w'),)
+    # plt.tight_layout()
+    # plt.savefig("src/data/plots/temp.png")
+
+
+if __name__ == "__main__":
+    clusters_table_filtered = polars.read_csv("src/data/results/combined_clusters_blasted_gtdb_filtered.tsv", separator='\t')
+    plot_taxonomic_distributions(clusters_table_filtered)
---
+++ src/scripts/train_test_split.py
@@ -0,0 +1,56 @@
+def filter_clusters_table():
+    clusters_table = polars.read_csv("src/data/results/combined_clusters_blasted_gtdb.tsv", separator='\t', infer_schema_length=600)
+    clusters_table = (
+        # add whether to select blast results or original data
+        clusters_table
+        .with_columns((polars.col("new_length").gt(polars.col("length")) & polars.col("new_percentage_in_puls").lt(polars.col("percentage_in_puls"))).alias("blast_status"))
+        .filter((polars.col("merged") == "merged") | polars.col("merged").is_null())
+    )
+
+    original_cols = [col for col in clusters_table.columns if not "new" in col]
+    fixed_cols = ["cluster_id", "tax_id", "database", "merged", "blast_status"]
+    new_cols = [col for col in clusters_table.columns if "new" in col]
+    rename_map = {old: old.replace("new", "").strip("_") for old in new_cols if "new" in old}
+
+    # get rows where blast_status is null or False, and select original columns
+    clusters_table_original = (
+        clusters_table
+        .filter(polars.col("blast_status").is_null())
+        .select(original_cols)
+        .vstack(
+            clusters_table
+            .filter(polars.col("blast_status") == False)
+            .select(original_cols)
+        )
+    )
+    # get rows to replace by blast results, replace with new columns
+    clusters_table_blasted = (
+        clusters_table
+        .filter(polars.col("blast_status") == True)
+        .select(fixed_cols + new_cols)
+        .rename(rename_map)
+        .select(original_cols)
+    )
+    clusters_table_full = clusters_table_original.vstack(clusters_table_blasted)
+
+    pul_lengths = (
+        clusters_table_full
+        .group_by('sequence_id')
+        .agg(
+            (polars.col('end') - polars.col('start')).sum().alias('pul_length_sum'),
+            polars.col('length').first(),
+        ) # length of all puls in sequence, full sequence length
+        .with_columns((100 * polars.col('pul_length_sum') / polars.col('length')).alias('percentage_in_puls')) # % of puls in genome
+        .select('sequence_id', 'length', 'pul_length_sum', 'percentage_in_puls') # select only relevant columns
+    )
+    # merge back with cluster table
+    clusters_table_filtered = (
+        clusters_table_full
+        .drop(['length', 'pul_length_sum', 'percentage_in_puls'])
+        .join(pul_lengths, on='sequence_id', how='left')
+        .sort("cluster_id")
+        .select(original_cols)
+        .filter(polars.col("percentage_in_puls")<50)
+    )
+    clusters_table_filtered.write_csv("src/data/results/combined_clusters_blasted_gtdb_filtered.tsv", separator='\t')
+    return clusters_table_filtered

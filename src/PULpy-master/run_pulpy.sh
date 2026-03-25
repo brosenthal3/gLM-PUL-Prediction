a93e63eba702b970c352e42e74e234fcca6fb38e
Rosenthal <brosenthal@res-hpc-lo02.researchlumc.nl>
2026-03-25 11:05:29 +0100

    adapt pulpy stuff

--- src/PULpy-master/run_pulpy.sh
+++ src/PULpy-master/run_pulpy.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-# FROM PULpy repository README.md
+# This script was adapted from README.md on the PULpy repository
 
 source ~/.bashrc
 mamba env create -f envs/PULpy.yaml
 mamba activate PULpy
 
 mkdir pfam_data && cd pfam_data
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.dat.gz
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/active_site.dat.gz
+wget --no-check-certificate ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
+wget --no-check-certificate ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.dat.gz
+wget --no-check-certificate ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/active_site.dat.gz
 gunzip Pfam-A.hmm.gz Pfam-A.hmm.dat.gz active_site.dat.gz
 hmmpress Pfam-A.hmm
 cd ..
 
 mkdir dbcan_data && cd dbcan_data
-wget http://bcb.unl.edu/dbCAN2/download/Databases/dbCAN-old@UGA/hmmscan-parser.sh
-wget http://bcb.unl.edu/dbCAN2/download/Databases/dbCAN-old@UGA/dbCAN-fam-HMMs.txt
+# NOTE: URLs had to be updated
+wget --no-check-certificate http://pro.unl.edu/dbCAN2/download/Databases/dbCAN-old-UGA/hmmscan-parser.sh
+wget --no-check-certificate http://pro.unl.edu/dbCAN2/download/Databases/dbCAN-old-UGA/dbCAN-fam-HMMs.txt.v14
 hmmpress dbCAN-fam-HMMs.txt
 chmod 755 hmmscan-parser.sh
 cd ..

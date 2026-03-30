d60bd12d93228290458d91a5f05bbb0ac254e583
Ben Rosenthal <benrosenthal03@gmail.com>
2026-03-30 14:28:28 +0200

    change blast script to blast full sequences

--- src/scripts/blast_truncated_sequences.py
+++ src/scripts/blast_truncated_sequences.py
@@ -7,165 +7,161 @@ import polars
 from Bio import Entrez, SeqIO
 from Bio.Blast import NCBIWWW, NCBIXML
 from tqdm import tqdm
+from utility_scripts import request_sequence
 
 
-def fetch_subsequence(accession, start, end):
-    """ Fetch full sequence in fasta format """
+class BlastHandler:
+    def __init__(self, email, cluster_table: polars.DataFrame, output_file: str):
+        Entrez.email = email
+        NCBIWWW.email = email
+        self.cluster_table = cluster_table
+        self.output_file = output_file
+        
 
-    print(f"Fetching {accession} from NCBI")
-    handle = Entrez.efetch(db="nucleotide", id=accession, rettype="fasta", retmode="text")
-    record = SeqIO.read(handle, "fasta")
-    handle.close()
+    def run_biopython_blast(self, fasta_path, taxid, ignore_taxonomy=False):
+        print(f"Running BLAST for {fasta_path.name}.")
+        fasta_string = fasta_path.read_text()
+        entrez_query = f"txid{taxid}[Organism]" if taxid is not None or ignore_taxonomy else None
 
-    # split into desired PUL subsequence
-    pul_subsequence = record.seq[start-1:end]
-    return record.id, pul_subsequence
+        result_handle = NCBIWWW.qblast(
+            "blastn",
+            "nt",
+            fasta_string,
+            megablast=True,
+            entrez_query=entrez_query,
+            hitlist_size=5,
+        )
 
+        blast_record = NCBIXML.read(result_handle)
+        result = []
+        for alignment in blast_record.alignments:
+            print("Alignment length: ", alignment.length)
+            if abs(alignment.length) > len(fasta_string): # only consider longer hits
+                sacc = alignment.accession
+                # check all HSPs
+                for hsp in alignment.hsps:
+                    sstart, send = hsp.sbjct_start, hsp.sbjct_end
+                    qstart, qend = hsp.query_start, hsp.query_end
+                    evalue = hsp.expect
+                    pident = 100.0 * hsp.identities / hsp.align_length
+                    q_coverage = 100.0 * (qend - qstart + 1) / len(fasta_string)
+                    qacc = blast_record.query.split('.')[0]
 
-def run_biopython_blast(fasta_path, taxid, ignore_taxonomy=False):
-    print(f"Running BLAST for {fasta_path.name}.")
-    fasta_string = fasta_path.read_text()
-    entrez_query = f"txid{taxid}[Organism]" if taxid is not None or ignore_taxonomy else None
+                    if float(pident) >= 99 and float(q_coverage) > 90 and qacc != sacc:
+                        print(f"Found hit: {sacc} for query {qacc}, pident={pident}, length={abs(int(send)-int(sstart))}")
+                        new_start = min(sstart, send)
+                        new_end = max(sstart, send)
+                        query_start = min(qstart, qend)
+                        query_end = max(qstart, qend)
 
-    result_handle = NCBIWWW.qblast(
-        "blastn",
-        "nt",
-        fasta_string,
-        megablast=True,
-        entrez_query=entrez_query,
-        hitlist_size=5,
-    )
+                        result.append((sacc, new_start, new_end, query_start, query_end, float(evalue), float(pident), alignment.length))
 
-    blast_record = NCBIXML.read(result_handle)
-    result = []
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            sacc = alignment.accession
-            sstart = hsp.sbjct_start
-            send = hsp.sbjct_end
-            evalue = hsp.expect
-            pident = 100.0 * hsp.identities / hsp.align_length
-            qacc = blast_record.query.split('.')[0]
-
-            if float(pident) >= 99 and qacc != sacc:
-                new_pul_range = (int(sstart), int(send)) # account for complementary strand hits
-                print(f"Found hit: {sacc} for query {qacc}, pident={pident}, length={int(send)-int(sstart)}")
-                result.append((sacc, min(new_pul_range), max(new_pul_range), float(evalue), float(pident)))
-
-    print(f"Found {len(result)} valid hits above 99% identity.")
-    # return hit with longest alignment
-    if result:
-        longest_result = max(result, key=lambda x: x[2] - x[1])
-        return longest_result
-    else:
-        return None
-
-
-def get_pul_info(output, accession, cluster_id, start, end):
-    for entry in output:
-        if entry["query_accession"] == accession:
-            shifted_bp = entry["subject_start"] - entry["query_start"]
-            # shift new subject accordingly to get new PUL range
-            s_start = start + shifted_bp
-            s_end = end + shifted_bp
-
-            return {
-                "cluster_id": cluster_id,
-                "query_accession": accession,
-                "query_start": start,
-                "query_end": end,
-                "tax_id": entry["tax_id"],
-                "subject_accession": entry["subject_accession"],
-                "subject_start": s_start,
-                "subject_end": s_end,
-                "evalue": None,
-                "pident": None
-            }
-
-    return {
-        "cluster_id": cluster_id,
-        "query_accession": accession,
-        "query_start": start,
-        "query_end": end,
-        "tax_id": None,
-        "subject_accession": "NO_HIT",
-        "subject_start": "NA",
-        "subject_end": "NA",
-        "evalue": "NA",
-        "pident": "NA"
-    }
+        print(f"Found {len(result)} valid hits above 99% identity and 90% query coverage.")
+        # return hit with longest alignment
+        if result:
+            longest_result = max(result, key=lambda x: x[7]) # sort by alignment length
+            return longest_result
+        else:
+            return None
 
 
-def get_blast_results(truncated_df, output_file, ignore_taxonomy):
-    # iterate over sequences
-    output = []
-    tried_accessions = set()
-    unsuccessful_accessions = []
-    if ignore_taxonomy:
-        print("Not taking taxid into account for query")
-
-    for row in tqdm(truncated_df.iter_rows(named=True), total=truncated_df.shape[0]):
-        accession = row["sequence_id"]
-        cluster_id = row["cluster_id"]
-        start = int(row["start"])
-        end = int(row["end"])
-        taxid = row["tax_id"]
-
-        # # handle cases where we already tried to fetch this accession
-        # if accession in tried_accessions:
-        #     print(f"Already tried {accession}, adding PUL info based on previous attempt")
-        #     output_dict = get_pul_info(output, accession, cluster_id, start, end)
-        #     output.append(output_dict)
-        #     continue
-
-        print(f"Processing {accession}:{start}-{end} (taxid {taxid})")
-        # fetch subsequence
-        seq_id, subseq = fetch_subsequence(accession, start, end)
+    def fetch_sequence(self, accession):
+        with Entrez.efetch(db="nucleotide", id=accession, rettype="fasta", retmode="text") as handle:
+            record = SeqIO.read(handle, "fasta")
 
         # save to temporary fasta file for blast query
         with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".fasta") as temp_file:
             fasta_path = Path(temp_file.name)
-            temp_file.write(f">{seq_id}_{cluster_id}_{start}_{end}\n{subseq}\n")
+            temp_file.write(f">{record.id}\n{record.seq}\n")
 
-        # run blast and parse output
-        # blast_output = run_blast(fasta_path, taxid)
-        # result = parse_filter_blast_output(blast_output)
-        try:
-            result = run_biopython_blast(fasta_path, taxid, ignore_taxonomy)
-        except Exception as e:
-            print(f"Error running BLAST for {accession}: {e}")
-            result = None
+        return fasta_path
 
-        fasta_path.unlink()
 
+    def save_result(self, result):
         # save result, save NA if no hit found
         if result:
-            sacc, sstart, send, evalue, pident = result
+            sacc, sstart, send, qstart, qend, evalue, pident, alignment_length = result
         else:
-            sacc, sstart, send, evalue, pident = "NO_HIT", "NA", "NA", "NA", "NA"
+            sacc, sstart, send, qstart, qend, evalue, pident, alignment_length = "NO_HIT", "NA", "NA", "NA", "NA", "NA", "NA", "NA"
             unsuccessful_accessions.append(accession)
 
-        output.append({
-            "cluster_id": cluster_id,
-            "query_accession": accession,
-            "query_start": start,
-            "query_end": end,
-            "tax_id": taxid,
-            "subject_accession": sacc,
-            "subject_start": sstart,
-            "subject_end": send,
-            "evalue": evalue,
-            "pident": pident
-        })
-        # write to file immediately to avoid losing results if script crashes and to keep track of progress
-        with open(output_file, "a") as f:
-            f.write(f"{cluster_id}\t{accession}\t{start}\t{end}\t{taxid}\t{sacc}\t{sstart}\t{send}\t{evalue}\t{pident}\n")
+        # output.append({
+        #     "query_accession": accession,
+        #     "query_start": qstart,
+        #     "query_end": qend,
+        #     "tax_id": taxid,
+        #     "subject_accession": sacc,
+        #     "subject_start": sstart,
+        #     "subject_end": send,
+        #     "evalue": evalue,
+        #     "pident": pident,
+        #     "alignment_length": alignment_length
+        # })
 
-        time.sleep(5) # being nice to NCBI
-        tried_accessions.add(accession)
-    
-    print(f"Finished processing. Unsuccessful accessions: {len(unsuccessful_accessions)}")
-    return output
+        # write to file immediately to avoid losing results if script crashes and to keep track of progress
+        with open(self.output_file, "a") as f:
+            f.write(f"{accession}\t{qstart}\t{qend}\t{taxid}\t{sacc}\t{sstart}\t{send}\t{evalue}\t{pident}\t{alignment_length}\n")
+
+
+    def get_blast_results(self):
+        # iterate over sequences
+        accessions_table = self.cluster_table.select("sequence_id", "tax_id").unique()
+
+        for row in tqdm(accessions_table.iter_rows(named=True), total=accessions_table.shape[0]):
+            accession = row["sequence_id"]
+            taxid = row["tax_id"]
+            print(f"Processing {accession} (taxid {taxid})")
+
+            # save sequence to fasta file
+            fasta_path = self.fetch_sequence(accession)
+
+            # run blast and parse output
+            try:
+                result = self.run_biopython_blast(fasta_path, taxid)
+            except Exception as e:
+                print(f"Error running BLAST for {accession}: {e}")
+                result = None
+            fasta_path.unlink()
+
+            self.save_result(result)
+            time.sleep(5) # being nice to NCBI
+        
+        print(f"Finished processing. Unsuccessful accessions: {len(unsuccessful_accessions)}")
+
+
+# def get_pul_info(output, accession, cluster_id, start, end):
+#     for entry in output:
+#         if entry["query_accession"] == accession:
+#             shifted_bp = entry["subject_start"] - entry["query_start"]
+#             # shift new subject accordingly to get new PUL range
+#             s_start = start + shifted_bp
+#             s_end = end + shifted_bp
+
+#             return {
+#                 "cluster_id": cluster_id,
+#                 "query_accession": accession,
+#                 "query_start": start,
+#                 "query_end": end,
+#                 "tax_id": entry["tax_id"],
+#                 "subject_accession": entry["subject_accession"],
+#                 "subject_start": s_start,
+#                 "subject_end": s_end,
+#                 "evalue": None,
+#                 "pident": None
+#             }
+
+#     return {
+#         "cluster_id": cluster_id,
+#         "query_accession": accession,
+#         "query_start": start,
+#         "query_end": end,
+#         "tax_id": None,
+#         "subject_accession": "NO_HIT",
+#         "subject_start": "NA",
+#         "subject_end": "NA",
+#         "evalue": "NA",
+#         "pident": "NA"
+#     }
 
 
 def main():
@@ -175,14 +171,14 @@ def main():
         "-i",
         "--input",
         type=str,
-        default="../data/truncated_genomes_test.tsv",
+        default="src/data/results/truncated_genomes.tsv",
         help="Input TSV file",
     )
     parser.add_argument(
         "-o", 
         "--output", 
         type=str, 
-        default="../data/blast_full_sequences.tsv",
+        default="src/data/results/blast_full_sequences.tsv",
         help="Output file"
     )
     parser.add_argument(
@@ -191,13 +187,8 @@ def main():
         default="b.rosenthal@lumc.nl",
         help="Email address required by NCBI Entrez",
     )
-    parser.add_argument(
-        "--ignore_taxonomy",
-        action="store_true"        
-    )
+
     args = parser.parse_args()
-    Entrez.email = args.email
-    NCBIWWW.email = args.email
     truncated_df = polars.read_csv(args.input, separator="\t")
 
     # try checking output file first to avoid unnecessary blast runs
@@ -209,10 +200,11 @@ def main():
     else:
         # create output file and write header
         with open(args.output, "w") as f:
-            f.write("cluster_id\tquery_accession\tquery_start\tquery_end\ttax_id\tsubject_accession\tsubject_start\tsubject_end\tevalue\tpident\n")
+            f.write("query_accession\tquery_start\tquery_end\ttax_id\tsubject_accession\tsubject_start\tsubject_end\tevalue\tpident\talignment_length\n")
 
     # get blast results for each sequence
-    output = get_blast_results(truncated_df, args.output, args.ignore_taxonomy)
+    Blaster = BlastHandler(args.email, truncated_df, args.output)
+    Blaster.get_blast_results()
 
 
 if __name__ == "__main__":

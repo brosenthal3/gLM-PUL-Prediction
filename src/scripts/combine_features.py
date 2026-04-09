03210667c32d1678833c67f6f8294e44b773b32c
Ben Rosenthal <benrosenthal03@gmail.com>
2026-04-09 16:55:06 +0200

    combine cayman and pfam feature tables

--- src/scripts/combine_features.py
+++ src/scripts/combine_features.py
@@ -1,10 +1,8 @@
 import polars
 
-df = polars.read_parquet("PUL.features.Pfam37.p1e-9.parquet")
-
 cazy = (
-    polars.scan_csv(
-        "PUL.features.cazy_cayman_v0.12.parquet",
+    polars.read_csv(
+        "src/data/genecat_output/genome.features.cayman_cazy_v0.12.0.csv",
         separator=",",
         has_header=True,
         schema_overrides={
@@ -13,19 +11,64 @@ cazy = (
             "domain_end": polars.UInt32,
             "family": polars.Utf8,
             "pvalue": polars.Float64,
+            "start": polars.UInt32,
+            "end": polars.UInt32
         }
     )
     .rename({
         "sequenceID": "protein_id",
         "family": "domain",
     })
-    .drop(["start", "end"])
     .with_columns(
-        hmm=polars.lit("Cayman_v0.12"),
-        i_evalue=polars.lit(0).cast(dtype=polars.Float64),
+        polars.lit("Cayman_v0.12.0").alias("hmm"),
+        polars.lit(0).cast(dtype=polars.Float64).alias("i_evalue"),
+        polars.col("protein_id").str.split("_").list.get(0).alias("sequence_id"),
+        polars.lit("+").alias("strand")
     )
+    .select([
+        "sequence_id",
+        "protein_id",
+        "start",
+        "end",
+        "strand",
+        "domain",
+        "hmm",
+        "i_evalue",
+        "pvalue",
+        "domain_start",
+        "domain_end"
+    ])
 )
-cazy.write_parquet("PUL.features.cazy_cayman_v0.12.parquet")
 
-joined = df.vstack(cazy).sort(by=["sequence_id", "domain_start"])
-joined.write_parquet("PUL.features.Pfam37.1_cazy_cayman_v0.12.p1e-9.parquet")
+cazy.write_parquet("src/data/genecat_output/genome.features.cayman.parquet")
+
+pfam_features = polars.read_parquet("src/data/genecat_output/genome.features.parquet")
+joined = pfam_features.vstack(cazy).sort(by=["sequence_id", "domain_start"])
+joined.write_parquet("src/data/genecat_output/genome.features.cayman.pfam.parquet")
+
+
+# cayman_schema = {
+#     'sequenceID': String, 
+#     'start': Int64, 
+#     'end': Int64, 
+#     'pvalue': Float64, 
+#     'family': String, 
+#     'annotLength': Int64, 
+#     'domain_start': Int64, 
+#     'domain_end': Int64}
+
+# pfam_schema = {
+#     'sequence_id': String, 
+#     'protein_id': String, 
+#     'start': UInt32, 
+#     'end': UInt32, 
+#     'strand': String, 
+#     'domain': String, 
+#     'hmm': String, 
+#     'i_evalue': Float64, 
+#     'pvalue': Float64, 
+#     'domain_start': UInt32, 
+#     'domain_end': UInt32
+# }
+# add: sequence_id, strand (+), hmm, i_evalue
+# drop: annotLength

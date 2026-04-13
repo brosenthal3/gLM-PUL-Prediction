c736de30862fab57306b075850fd830cf972bd6d
Ben Rosenthal <b.rosenthal@lumc.nl>
2026-04-13 18:03:29 +0200

    bug fixes for dbcan hmms

--- src/scripts/genecat_preprocess.sh
+++ src/scripts/genecat_preprocess.sh
@@ -3,17 +3,11 @@
 # best to run in interactive session
 export PYTHONPATH='/exports/archive/lucid-grpzeller-primary/hackett/GeneCat/src'
 PFAM=/exports/archive/lucid-grpzeller-primary/hackett/GeneCat/data/hmms/Pfam/Pfam-A.Pfam37.1.h3m
-VOCAB=/exports/archive/lucid-grpzeller-primary/hackett/GeneCat/data/data_split_class_level/models_multilabel_models/jan_model/BERT_train.fold_0.all_domains.min50.vocab.txt
+CAZY=/exports/archive/lucid-grpzeller-primary/hackett/GeneCat/data/hmms/CAZy/dbCAN/dbCAN-HMMdb-V14.h3m
+#VOCAB=/exports/archive/lucid-grpzeller-primary/hackett/GeneCat/data/data_split_class_level/models_multilabel_models/jan_model/BERT_train.fold_0.all_domains.min50.vocab.txt
 OUT=/exports/lucid-grpzeller-work/brosenthal/gLM-PUL-Prediction/src/data/genecat_output/
 GENOMES=/exports/lucid-grpzeller-work/brosenthal/gLM-PUL-Prediction/src/data/genomes/combined_genomes.gb
 
-python -m genecat.cli preprocess -i $GENOMES --hmms $PFAM -v $VOCAB -o $OUT --write-tables --call-genes --index-db --write-tables --unique-domains
+python -m genecat.cli preprocess -i $GENOMES --hmms $PFAM -o $OUT --write-tables --call-genes --hmm-type pfam --output-name pfam
+python -m genecat.cli preprocess -i $GENOMES --hmms $CAZY -o $OUT --write-tables --call-genes --hmm-type cazy --output-name dbcan
 python -m genecat.cli call-genes -i $GENOMES -o $OUT --type faa+gff
-
-#export PYTHONPATH='/exports/archive/lucid-grpzeller-primary/hackett/software/cayman'
-#CUTOFFS=/exports/archive/lucid-grpzeller-primary/hackett/GeneCat/data/hmms/CAZy/Cayman/cutoffs.csv
-#CAZY_HMMS=/exports/archive/lucid-grpzeller-primary/hackett/GeneCat/data/hmms/CAZy/Cayman/hmms_bin/cayman.v3.seed42_selected.h3m
-#NUM_THREADS=16 # by default it will run just one thread
-#PROTEINS=$OUT/genome.genes.faa
-
-#python -m cayman annotate_proteome $CAZY_HMMS $PROTEINS --cutoffs $CUTOFFS -o $OUT/genome.features.cayman_cazy_v0.12.0.csv -t $NUM_THREADS
--- src/scripts/test_hmm_stuff.py
+++ src/scripts/test_hmm_stuff.py
@@ -13,13 +13,13 @@ def _save_hmm_file(model_path, hmms):
     hmm_out_file = f"{model_path}/features"
     model_features = polars.read_csv(f"{model_path}/domains.tsv", separator='\t')
     # open and save hmms
-    hmms_to_save = HMMloader.read_hmms(hmmdb=hmms, whitelist=model_features.to_series().to_list())
+    hmms_to_save = HMMLoader.read_hmms(hmmdb_path=Path(hmms), whitelist=model_features.to_series().to_list())
     hmms_to_save.write_to_h3m_file(hmm_out_file)
 
     return hmm_out_file + ".selected.h3m"
 
 
 if __name__ == "__main__":
-    model_path = "src/data/results/gecco/fold_0/model"
+    model_path = "src/data/results/gecco/model_0"
     hmms = "src/data/hmms"
     print(_save_hmm_file(model_path, hmms))

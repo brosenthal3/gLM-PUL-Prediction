01fb6edcc72ff8be675fe8e68e7bb5df3e009277
Ben Rosenthal <b.rosenthal@lumc.nl>
2026-04-20 10:53:33 +0200

    add test esmc embeddings, adapt esmc script

--- src/scripts/esmc_script.py
+++ src/scripts/esmc_script.py
@@ -9,7 +9,8 @@ from tqdm import tqdm
 import torch
 
 faa_path = "src/data/genecat_output/genome.genes.faa"
-client = ESMC.from_pretrained("esmc_300m").to("cuda") # or "cpu"
+#sequences = polars.read_csv("src/data/results/cblaster_results.tsv", separator="\t", infer_schema_length=700).select("sequence_id").unique().to_series().to_list()
+client = ESMC.from_pretrained("esmc_300m").to("cpu") # or "cpu"
 
 with open(faa_path, "r") as f:
     faa_iter = SimpleFastaParser(f)
@@ -19,21 +20,25 @@ with open(faa_path, "r") as f:
     embeddings = []
 
     for gene_id, seq in tqdm(faa_iter, total=772451):
+#        if gene_id.split("_")[0] not in sequences:
+#            continue
+
         protein = ESMProtein(sequence=seq)
         protein_tensor = client.encode(protein)
         # NOTE that the track is sequence - there are other tracks too!
         # NOTE that return_mean_embedding doesnt seem to work. still get None
         logits_output = client.logits(
-            protein_tensor, LogitsConfig(sequence=True, return_mean_embedding=True, return_embeddings=True)
+            protein_tensor, LogitsConfig(sequence=True, return_embeddings=True)
         )
         protein_ids.append(gene_id)
         seq_length.append(len(seq))
         embeddings.append(logits_output.embeddings.mean(dim=1).squeeze(0).type(torch.float32).cpu().numpy())
 
-emb_series = polars.Series("seq_embeddings", embeddings)
-seq_length_series = polars.Series("seq_length", seq_length)
+emb_series = polars.Series("embeddings", embeddings)
+seq_length_series = polars.Series("protein_length", seq_length)
 gene_series = polars.Series("protein_id", protein_ids)
 df = polars.DataFrame([gene_series, seq_length_series, emb_series])
-print(df.schema)
 os.makedirs("src/data/results/esmc", exist_ok=True)
+print(df)
+print(df.schema)
 df.write_parquet("src/data/results/esmc/esmc_embeddings.parquet")

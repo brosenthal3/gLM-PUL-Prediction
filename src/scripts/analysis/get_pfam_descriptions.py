7f95c204a9bd3ae51829273ddb134a59462a1841
Ben Rosenthal <b.rosenthal@lumc.nl>
2026-05-13 13:50:58 +0200

    add pfam descriptions and volcano plot of feature enrichment

---
+++ src/scripts/analysis/get_pfam_descriptions.py
@@ -0,0 +1,59 @@
+import requests
+import polars
+from tqdm import tqdm
+from time import sleep
+import os
+
+def get_pfam_description(pfam_id):
+    try:
+        url = f"https://www.ebi.ac.uk/interpro/api/entry/pfam/{pfam_id}"
+        r = requests.get(url, timeout=5)
+
+        if r.status_code != 200:
+            print("Status code not 200")
+            return (None, None)
+
+        data = r.json()
+        name = data["metadata"]["name"].get("name", None)
+        description = data["metadata"]["description"]
+        if description is not None:
+            description = description[0].get("text", None).strip("<p>").strip("</p>")
+
+        return name, description
+
+    except Exception as e:
+        print(e)
+        print(f"Error fetching description for {pfam_id}")
+        return (None, None)
+
+
+def get_pfam_descriptions(domains_df):
+    domains = domains_df["domain"].unique().to_list()
+    print(f"Fetching descriptions for {len(domains)} PFAM domains...")
+
+    pfam_metadata = []
+    for domain in tqdm(domains):
+        name, desc = get_pfam_description(domain)
+        pfam_metadata.append({
+            "domain": domain,
+            "name": name,
+            "description": desc
+        })
+        sleep(0.05) # be nice to the API
+
+    pfam_metadata_df = polars.DataFrame({
+        "domain": domains,
+        "name": [desc["name"] for desc in pfam_metadata],
+        "description": [desc["description"] for desc in pfam_metadata]
+    })
+    os.makedirs("src/data/analysis", exist_ok=True)
+    pfam_metadata_df.write_parquet("src/data/analysis/pfam_metadata.parquet")
+
+if __name__ == "__main__":
+    features = polars.read_parquet("src/data/genecat_output/pfam.features.parquet")
+    sequences = polars.read_csv("src/data/data_collection/clusters_deduplicated_cblaster.tsv", separator="\t")
+
+    print(features)    
+    selected_features = features.join(sequences.select("sequence_id"), how="semi", on="sequence_id")
+    print(selected_features)
+    get_pfam_descriptions(selected_features)
--- src/scripts/visualization/analyze_features.py
+++ src/scripts/visualization/analyze_features.py
@@ -3,45 +3,40 @@ import matplotlib.pyplot as plt
 import polars
 import numpy as np
 import os
-import gb_io
 import argparse
 from scipy.stats import fisher_exact
 from statsmodels.stats.multitest import multipletests
-import plotly.express as px
-import requests
 from tqdm import tqdm
-from time import sleep
 
 def analyze_domain_enrichment(features_with_predictions, label_column="is_PUL_pred"):
+    features_with_predictions = features_with_predictions.select("domain", label_column)
     total_positive = (features_with_predictions.select(polars.col(label_column).sum()).item())
     total_negative = len(features_with_predictions) - total_positive
 
     results = []
-    for domain in features_with_predictions["domain"].unique():
+    for domain in tqdm(features_with_predictions["domain"].unique(), desc="Computing enrichments"):
         subset = features_with_predictions.filter(polars.col("domain") == domain)
 
-        # Domain present
-        a = subset.select(polars.col(label_column).sum()).item()
-        b = len(subset) - a
+        # create contingency table
+        present_positives = subset.select(polars.col(label_column).sum()).item()
+        present_negatives = len(subset) - present_positives
+        absent_positives = total_positive - present_positives
+        absent_negatives = total_negative - present_negatives
+        contingency = [[present_positives, present_negatives], [absent_positives, absent_negatives]]
 
-        # Domain absent
-        c = total_positive - a
-        d = total_negative - b
-
-        # Fisher exact test
-        contingency = [[a, b], [c, d]]
+        # perform fishers exact test on the contingency table
         oddsratio, pvalue = fisher_exact(contingency)
 
-        # log2 enrichment
+        # calculate log2 enrichment
         log2_enrichment = np.log2(
-            ((a + 0.5) / (a + b + 1)) /
-            ((c + 0.5) / (c + d + 1))
+            ((present_positives + 0.5) / (present_positives + present_negatives + 1)) /
+            ((absent_positives + 0.5) / (absent_positives + absent_negatives + 1))
         )
 
         results.append({
             "domain": domain,
-            "positive_with_domain": a,
-            "negative_with_domain": b,
+            "positive_with_domain": present_positives,
+            "negative_with_domain": present_negatives,
             "oddsratio": oddsratio,
             "log2_enrichment": log2_enrichment,
             "pvalue": pvalue,
@@ -52,13 +47,12 @@ def analyze_domain_enrichment(features_with_predictions, label_column="is_PUL_pr
     fdr = multipletests(stats_df["pvalue"], method="fdr_bh")[1]
     stats_df = stats_df.with_columns([
         polars.Series("fdr", fdr),
-        (-np.log10(polars.Series(fdr))).alias("neglog10_fdr"),
+        (-np.log10(polars.Series(fdr))).clip(upper_bound=400).alias("neglog10_fdr"),
     ])
 
-    # add significance column 
+    # add significance column
     stats_df = stats_df.with_columns([((polars.col("fdr") < 0.05) & (polars.col("log2_enrichment").abs() > 1)).alias("significant")])
     return stats_df
-
 
 
 def main(args):
@@ -73,7 +67,12 @@ def main(args):
     model_evaluator.aggregate_all_folds()
     model_evaluator.recompute_predictions(0, threshold=0.2) # recompute labels at threshold
 
-    features = polars.read_parquet("src/data/genecat_output/pfam.features.parquet")
+    feature_descriptions = polars.read_parquet("src/data/analysis/pfam_metadata.parquet")
+    if args.features == "pfam":
+        features = polars.read_parquet("src/data/genecat_output/pfam.features.parquet")
+    elif args.features == "cazy":
+        features = polars.read_parquet("src/data/genecat_output/dbcan.pfam.features.parquet")
+
     features_with_predictions = (
         model_evaluator.labeled_results[0]
         .select("protein_id", "is_PUL_pred", "is_PUL_pulpy", "is_PUL")
@@ -82,61 +81,123 @@ def main(args):
             how="inner",
             on="protein_id"
         )
+        .join(
+            feature_descriptions,
+            on="domain",
+            how="left"
+        )
     )
 
     label_columns = ["is_PUL_pred", "is_PUL_pulpy", "is_PUL"]
-    all_results = {}
+    titles = [
+        f"Predicted PULs ({model_evaluator.model_name})",
+        "PULpy PULs",
+        "Experimental PULs"
+    ]
+    fig, axs = plt.subplots(2, 3, figsize=(16, 10), height_ratios=[3, 2])
 
-    for label in label_columns:
+    for i, label in enumerate(label_columns):
         # get stats
-        stats_df = analyze_domain_enrichment(features_with_predictions, label)
-        all_results[label] = stats_df
-
+        stats_df = (
+            analyze_domain_enrichment(features_with_predictions, label)
+            .join(
+                features_with_predictions.select(["domain", "name"]).unique(),
+                on="domain",
+                how="left"
+            )
+        )
         # convert to pandas for plotting
         pdf = stats_df.to_pandas()
 
-        # Label only strong hits
-        pdf["label"] = np.where(((pdf["fdr"] < 0.01) & (np.abs(pdf["log2_enrichment"]) > 2)), pdf["domain"], "")
-
-        fig = px.scatter(
-            pdf,
-            x="log2_enrichment",
-            y="neglog10_fdr",
-            color="significant",
-            size="positive_with_domain",
-            hover_data=[
-                "domain",
-                "positive_with_domain",
-                "oddsratio",
-                "pvalue",
-                "fdr",
-            ],
-            text="label",
-            title=f"Pfam enrichment volcano plot: {label}",
+        # scatter plot
+        ax = axs[0, i]
+        scatter = ax.scatter(
+            pdf["log2_enrichment"],
+            pdf["neglog10_fdr"],
+            s=np.clip(pdf["positive_with_domain"], 5, 100), # use size to indicate how many PUL proteins have this domain
+            alpha=0.65,
         )
 
-        # threshold lines
-        fig.add_vline(x=-1, line_dash="dash")
-        fig.add_vline(x=1, line_dash="dash")
+        ax.axvline(x=-1, linestyle="--")
+        ax.axvline(x=1, linestyle="--")
+        ax.axhline(y=-np.log10(0.05), linestyle="--")
+        significant = pdf[(pdf["neglog10_fdr"] > 100) & (np.abs(pdf["log2_enrichment"]) > 2)]
 
-        fig.add_hline(y=-np.log10(0.05), line_dash="dash")
+        for _, row in significant.iterrows():
+            ax.text(
+                row["log2_enrichment"],
+                row["neglog10_fdr"],
+                row["domain"],
+                fontsize=8,
+            )
 
-        fig.update_traces(textposition="top center")
+        if i == 0:
+            ax.set_ylabel("-log10(FDR)")
+            ax.scatter([], [], s=10, label="Found in 10 PUL proteins", color="gray")
+            ax.scatter([], [], s=50, label="Found in 50 PUL proteins", color="gray")
+            ax.scatter([], [], s=100, label="Found in >100 PUL proteins", color="gray")
+            ax.legend(loc="upper left")
+    
+        ax.set_xlabel("log2 enrichment")
+        ax.set_title(titles[i])
 
-        fig.update_layout(
-            height=900,
-            width=1200,
-            xaxis_title="log2 enrichment",
-            yaxis_title="-log10(FDR)",
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+
+        # add bar plot of top domains
+        bar_ax = axs[1, i]
+        top_domains = (pdf.sort_values("neglog10_fdr", ascending=False).head(10).copy())
+        # add label for name
+        top_domains["label"] = np.where(
+            top_domains["name"].notna(),
+            top_domains["name"],
+            top_domains["domain"]
         )
+        top_domains = top_domains.iloc[::-1]
+        bars = bar_ax.barh(
+            top_domains["domain"],
+            top_domains["neglog10_fdr"]
+        )
+        for bar, (_, row) in zip(bars, top_domains.iterrows()):
+            width = bar.get_width()
+            y = bar.get_y() + bar.get_height() / 2
+            x = 10
+            ha = "left"
+            color = "black"
 
-        fig.write_html(os.path.join(model_evaluator.output_path, f"pfam_enrichment_{label}.html"))
+            bar_ax.text(
+                x,
+                y,
+                row["label"],
+                va="center",
+                ha=ha,
+                fontsize=9,
+                color=color
+            )
 
+        if i == 0:
+            bar_ax.set_ylabel("Pfam domain")
+
+        bar_ax.set_xlabel("PUL genes with domain")
+        if i == 1:
+            bar_ax.set_title(f"Top enriched domains")
+
+        bar_ax.spines["top"].set_visible(False)
+        bar_ax.spines["right"].set_visible(False)
+
+        # Smaller labels
+        bar_ax.tick_params(axis='y', labelsize=7)
+
+    fig.suptitle("Volcano plots of Pfam domains enrichment")
+    fig.tight_layout()
+    fig.savefig(os.path.join(model_evaluator.output_path, f"pfam_enrichment_{label}.png"))
+    plt.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Visualize Pfam domain enrichment in model predictions"
+        description="Visualize feature (pfam/cazy) enrichment in model predictions"
     )
     parser.add_argument("--model", type=str, help="Name of model to evaluate", required=True)
+    parser.add_argument("--features", type=str, help="Type of feature set to analyze. Options: pfam, cazy", default="pfam")
     args = parser.parse_args()
     main(args)
